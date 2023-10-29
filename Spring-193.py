c37325dca18fb655a1f6514e87965258b401bbdf
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-10-29 07:46:16 +0000

    Add files via upload

--- Spring-193.py
+++ Spring-193.py
@@ -361,7 +361,7 @@ class compression:
                                      c=1
                                      
                                      if c==1:
-                                         while counts_11!=100:
+                                         while counts_11!=10:
                                              L=0
                                              long2=len(INFO_OR_DATA_TO_BINARY)
                                              block=0
@@ -455,7 +455,7 @@ class compression:
                                              INFO_OR_DATA_TO_BINARY=INFOS
                                              #print(INFOS)
                                              
-                                         if counts_11==100:
+                                         if counts_11==10:
                                     
 
                                               INFO_OR_DATA_TO_BINARY="1"+INFOS
