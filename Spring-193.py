1c7ef51c7a06bbc415cdec87aa53e957650e65c2
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-10-30 11:03:18 +0000

    Add files via upload

--- Spring-193.py
+++ Spring-193.py
@@ -368,7 +368,6 @@ class compression:
                                              X=0
                                              X1=0
                                              INFOS=""
-                                             Er="0"
                                              
                                              while block<long2:
                                                 xyz1=INFO_OR_DATA_TO_BINARY[block:block+5]
@@ -415,9 +414,8 @@ class compression:
                                                     #print(C)   
                                                     
                                                 elif A==X and B==X and X==0:
-                                                    Er="1"
                                                     
-
+                                                    print("Error wrong compression  if A==X and B==X and X==0:")
                                                     #print(xyz1)
                                                     #print(C)   
                                                     
@@ -432,7 +430,7 @@ class compression:
 
                                                  
                                                          #print(X)
-                                                         C=xyz1
+                                                         C=xyz1[:4]
                                                          
                                                        
                                                          #print(C)
@@ -442,7 +440,7 @@ class compression:
                                                  
                                                 if A==X and B=="00" and X==1 and  len(xyz1)==5:
                                                     if xyz1[4:5]=="0":
-                                                        Er="1"
+                                                        print("Error wrong compression if A==X and B==00 and X==1 and  len(xyz1)==5:")
                                                     #print(xyz1)
                                                     #print(C)   
                                                                                                                                                                                                      
@@ -461,15 +459,11 @@ class compression:
                                              L-=1
                                            
                                              C=format(L,'02b')
-                                             if Er=="0":
-                                                 INFOS=INFOS+C+Er
-                                             else:
-                                                  INFOS=INFO_OR_DATA_TO_BINARY+Er
-                                                  
+                                             INFOS=INFOS+C
                                              INFO_OR_DATA_TO_BINARY=INFOS
                                              After_long=len(INFOS)
                                              counts_12+=1
-                                             if After_long>long2  or counts_12==(2**24)-1:
+                                             if After_long>long2 or counts_12==(2**24)-1:
                                                  counts_11=1
                                              #print(INFOS)
                                              
